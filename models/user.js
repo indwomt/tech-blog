f5890289b505e909e1927c2293b980a1baf3757d
Lauren Bentley <laurendebentley@gmail.com>
2023-02-26 16:42:01 -0500

    single post template added

--- models/user.js
+++ models/user.js
@@ -1,47 +1,54 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection')
 
-class User extends Model {}
-
-User.init({
-    id: {
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
+  
+  User.init(
+    {
+      id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
-        autoIncrement: true
-    },
-
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-
-    email: {
+        autoIncrement: true,
+      },
+      name: {
         type: DataTypes.STRING,
         allowNull: false,
-        // only allow one account per email
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
         unique: true,
         validate: {
-            isEmail: true,
-        }
-
-    },    
-    
-    password: {
+          isEmail: true,
+        },
+      },
+      password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-            len: [8],
-        }
-    }    
-},
-
-{
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  }
-)
-
-module.exports = User
+          len: [8],
+        },
+      },
+    },
+    {
+      hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user',
+    }
+  );
+  
+  module.exports = User;
+  
