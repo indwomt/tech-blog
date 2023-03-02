422076581cc219942030f43f036e9b6b3ddf0926
Lauren Bentley <laurendebentley@gmail.com>
2023-03-02 13:06:39 -0500

    still troubleshooting

--- models/index.js
+++ models/index.js
@@ -2,6 +2,31 @@ const Comment = require('./comment')
 const User = require('./user');
 const Post = require('./post')
 
+
+
+
+User.hasMany(Post, {
+    foreignKey: `user_id`
+})
+Post.belongsTo(User, {
+    foreignKey: `user_id`
+})
+User.hasMany(Comment, {
+    foreignKey: `user_id`
+})
+Comment.belongsTo(User, {
+    foreignKey: `user_id`
+})
+Post.hasMany(Comment, {
+    foreignKey: `post_id`
+})
+Comment.belongsTo(Post, {
+    foreignKey: `post_id`
+})
+
+
+
+
 module.exports = {
     Comment,
     User,
