659a4a62db1714d97466c4ac6255fabaa319ab58
Lauren Bentley <laurendebentley@gmail.com>
2023-03-04 15:59:11 -0500

    commit

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -84,7 +84,7 @@ router.get('/:id', async (req, res) => {
 
 
 
-router.delete('/:id', withAuth, (req, res) => {
+router.delete('/:id',  (req, res) => {
   Post.destroy({
           where: {
               id: req.params.id,
